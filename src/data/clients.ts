f52e635516c2c76c0c7a56a561ba0203810e6815
hakim <125070824+anynonenom@users.noreply.github.com>
2026-08-05 14:52:42 +0100

    Add Logo's Clients

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -3,6 +3,8 @@ import dmc from "@/assets/dmc.png";
 import educazenKids from "@/assets/educazenkids.png";
 import eidenAcademyLogo from "@/assets/eiden-academy-logo.png";
 import lunjaVillage from "@/assets/lunja-village.png";
+import onereetail from "@/assets/onereetail.png";
+import sous_drougrie from "@/assets/souss-drougerie.png";
 
 export type Client = {
   name: string;
@@ -13,6 +15,8 @@ export const eidenClients: Client[] = [
   { name: "Bôpassage", logo: bopassage },
   { name: "DMC Hospitality Morocco", logo: dmc },
   { name: "EducazenKids", logo: educazenKids },
-  { name: "Eiden Academy", logo: eidenAcademyLogo },
   { name: "Lunja Village", logo: lunjaVillage },
+  { name: "OneRetail", logo: onereetail },
+  { name: "Sous Drougrie", logo: sous_drougrie },
+
 ];
